8e25aaba820064fb4507657a50689db5d27a3964
Manohar Reddy <f20180564@hyderabad.bits-pilani.ac.in>
2020-04-14 09:43:47 +0530

    Add all buttons

--- remote.py
+++ remote.py
@@ -4,8 +4,47 @@ hostIP = '192.168.0.6'
 
 class Tatasky:
 	
-	buttonMute = bytes.fromhex('04 01 00 00 00 00 e0 05 04 00 00 00 00 00 e0 05')
-	buttonSelect = bytes.fromhex('04 01 00 00 00 00 e0 01 04 00 00 00 00 00 e0 01')
+	buttonPower = 'e0 00'
+	buttonSelect = 'e0 01'
+	buttonBack = 'e0 02'
+	buttonVolumeUp = 'e0 03'
+	buttonVolumeDown = 'e0 04'
+	buttonMute = 'e0 05'
+	buttonChannelUp = 'e0 06'
+	buttonChannelDown = 'e0 07'
+	buttonApps = 'e0 08'
+	buttonHelp = 'e0 09'
+	buttonSettings = 'e0 0a'
+	buttonGuide = 'e0 0b'
+	buttonInfo = 'e0 0e'
+	buttonHome = 'e0 10'
+	buttonUp = 'e1 00'
+	buttonDown = 'e1 01'
+	buttonLeft = 'e1 02'
+	buttonRight = 'e1 03'
+	buttonRed = 'e2 00'
+	buttonGreen = 'e2 01'
+	buttonYellow = 'e2 02'
+	buttonBlue = 'e2 03'
+	button0 = 'e3 00'
+	button1 = 'e3 01'
+	button2 = 'e3 02'
+	button3 = 'e3 03'
+	button4 = 'e3 04'
+	button5 = 'e3 05'
+	button6 = 'e3 06'
+	button7 = 'e3 07'
+	button8 = 'e3 08'
+	button9 = 'e3 09'
+	buttonPlay = 'e4 00'
+	buttonPause = 'e4 01'
+	buttonRecord = 'e4 03'
+	buttonForward = 'e4 04'
+	buttonRewind = 'e4 06'
+	buttonTV = 'ef 00'
+	buttonFavs = 'ef 01'
+	buttonPlan = 'ef 02'
+	buttonOnDemand = 'ef 03'
 
 	def __init__(self, p_hostIP, p_hostPORT = 5900):
 		self.ip = p_hostIP
@@ -14,7 +53,7 @@ class Tatasky:
 		self.socket = self.__createSocket()
 
 	def click(self, button):
-		self.socket.send(button)
+		self.socket.send(bytes.fromhex(f'04 01 00 00 00 00 {button} 04 00 00 00 00 00 {button}'))
 
 
 	def __createSocket(self):
@@ -41,4 +80,4 @@ class Tatasky:
 if __name__ == '__main__':
 
 	t = Tatasky(hostIP)
-	t.click(Tatasky.buttonMute)
+	t.click(Tatasky.buttonSelect)
